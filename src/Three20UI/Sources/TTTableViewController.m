0d4f2f3948351e654e309ff8b374af65c8b7fd5f
Benedikt Meurer <benedikt.meurer@googlemail.com>
2011-06-22 19:56:23 +0200

    Don't scroll to newly inserted rows in TTTableViewController.
    In -model:didInsertObject:atIndexPath: of TTTableViewController, the
    -scrollToRowAtIndexPath:atScrollPosition:animated: message was send
    to the tableView whenever a new row was inserted. This is not going
    to work if the new row was inserted as part of a larger change, i.e.
    if -beginUpdates was called on the tableView and some new sections
    were inserted prior to inserting the row.
    
    In addition, scrolling to arbitrary rows without explicit control
    from the App developer isn't good practice either, so just ditch
    the scroll code all together.

--- src/Three20UI/Sources/TTTableViewController.m
+++ src/Three20UI/Sources/TTTableViewController.m
@@ -586,9 +586,6 @@
             TTDCONDITIONLOG(TTDFLAG_TABLEVIEWMODIFICATIONS, @"INSERTING ROW AT %@", newIndexPath);
             [_tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath]
                               withRowAnimation:UITableViewRowAnimationTop];
-
-            [_tableView scrollToRowAtIndexPath:newIndexPath
-                              atScrollPosition:UITableViewScrollPositionNone animated:NO];
           }
           [self invalidateView];
 
